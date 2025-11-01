c0afdcb42d80ecbe98cc76c8c188c3d8dbdad9f2
douknowiloveyou <i70766707@gmail.com>
2025-11-01 09:13:59 +0700

    Add c# support

---
+++ src/language/cs.py
@@ -0,0 +1,55 @@
+semantic_token = {
+    "method:csharp": {
+        "foreground": "#787878",
+    },
+}
+
+
+fg1 = []
+
+fg2 = [
+    "variable.other.object.property.cs",
+]
+
+fg3 = []
+
+green = [
+    "comment.line.double-slash.cs",
+]
+
+yellow = [
+    "keyword.type.void.cs",
+    "entity.name.type.cs",
+    "keyword.type.double.cs",
+]
+
+purple = [
+    "entity.name.variable.parameter.cs",
+]
+
+orange = [
+    "punctuation.terminator.statement.cs",
+    "keyword.operator.assignment.cs",
+    "keyword.operator.arithmetic.cs",
+]
+
+red = [
+    "string.quoted.double.cs",
+    "constant.numeric.decimal.cs",
+]
+
+dark_blue = [
+    "keyword.other.directive.using.cs",
+    "storage.type.namespace.cs",
+]
+
+blue = [
+    "storage.modifier.public.cs",
+    "storage.modifier.private.cs",
+    "storage.modifier.override.cs",
+    "keyword.control.flow.return.cs",
+    "keyword.control.conditional.if.cs",
+    "storage.type.var.cs",
+]
+
+gray = []
